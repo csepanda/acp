ddb750c2362df7ab0b2cdd167c1ed6d88f3b1a73
Andrey Bova <andrewbova@yandex.ru>
2017-01-29 00:02:32 +0300

    Add function-like macro preprocessing

--- src/Macro.pm
+++ src/Macro.pm
@@ -43,6 +43,36 @@ sub sub_define_object_like($$$) {
     return ($$src =~ s/\b$macro_name\b/$macro_body/g);
 }
 
+=item B<sub_define_function_like($$$$)>
+Substitutes function-like macro's name in the code with macro's body.
+    Argument 0 is a link to string of source-code
+    Argument 1 is a macro's name
+    Argument 2 is a macro's arguments
+    Argument 3 is a macro's body
+    Returns  count of substitution
+=cut
+
+sub sub_define_function_like($$$$) {
+    my $src        = shift // return undef;
+    my $macro_name = shift // return undef;
+    my @macro_args = @{ shift  @_ };
+    my $macro_body = shift // return undef;
+    my $brackets    = '(\(([^)(]*?(?1)?[^)(]*?)+\))';
+
+    while ($$src =~ m/\b$macro_name$brackets/) {
+        my $tmp = $1;
+        $tmp =~ s/^\(|\)$//g;
+        my @args = split(/\s*,\s*/, $tmp);
+        my $macro = $macro_body;
+        return undef if scalar @args != scalar @macro_args;
+        for (my $i = 0; $i < scalar @args; $i++) {
+            $macro =~ s/(?<!#)#\s*$macro_args[$i]/"$args[$i]"/g;
+            $macro =~ s/(\b|##\s*)$macro_args[$i](\b|\s*##)/$args[$i]/g;
+        }
+        $$src =~ s/$macro_name\($tmp\)/$macro/;
+    }
+    return ($$src =~ s/\b$macro_name\b/$macro_body/g);
+}
 
 =head1 AUTHOR
 
